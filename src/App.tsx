dfad2ad3353a7445fc38db51934c3a4859b5795b
f <f>
2021-04-14 21:04:21 +0200

    Reveal npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
+// @ts-ignore
+import Zoom from 'react-reveal/Zoom';
 import './App.css';
 
 const App: React.FC = () =>{
@@ -7,10 +9,12 @@ const App: React.FC = () =>{
   const [showText, setShowText] = useState(false);
 
   return <div className="parent">
-
+  <Zoom>
     <div className="section" >
       <h1>Hello</h1>
     </div>
+    </Zoom>
+
 
   </div>
     
