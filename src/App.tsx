5ed687f651dbd9cb4aaed5824d897873f7e30c0f
f <f>
2021-04-15 20:24:38 +0200

    First draft
    First draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 // @ts-ignore
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 // @ts-ignore
-import Zoom, { Fade } from 'react-reveal';
+import Zoom, { Flip } from 'react-reveal';
 import './App.css';
 import Skills from './Skills';
 import Projects from './Projects';
 
 const App: React.FC = () => {
 
-  const [showText, setShowText] = useState(false);
-
   return <div className="container">
 
     <Router>
       <div className="parent">
         <div className="section" >
-
-          <Link to="/login" style={{ textDecoration: 'none' }} className="linkText"><h3>Summary</h3></Link>
-          <Link to="/projects"style={{ textDecoration: 'none' }} className="linkText"><h3>Projects</h3></Link>
-
-
-
+        <Flip top>
+          <h1>Felix Ahlcrona</h1>
+        </Flip>
+          <Link to="/summary" style={{ textDecoration: 'none' }} className="linkText"><h3>Overview</h3></Link>
+          <Link to="/projects"style={{ textDecoration: 'none' }} className="linkText"><h3>Skillset</h3></Link>
         </div>
 
         <div className="section" >
+       
           <Switch>
-            <Route path="/login">
+            <Route path="/summary">
               <Skills />
             </Route>
             <Route path="/projects">
               <Projects />
-            </Route>
+            </Route>     
           </Switch>
           <Zoom>
         
